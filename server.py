2d8e27db00c5f3468f17d77036d34d6d7839c191
serbansandu <serban.sandu@mobly.be>
2021-05-13 18:44:02 +0300

    Add request validation

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import hug
 
-from nest import rebuild_json
+from nest import rebuild_json, validate_args, MissingArguments, InvalidArgument, TooManyArguments, AlreadyPassedArgument
 
 
 @hug.post(
@@ -13,4 +13,14 @@ from nest import rebuild_json
 @hug.local()
 def restructure(body, keys: hug.types.delimited_list(",")):
     """Restructures list of dicts to a single dict with arbitrary level of nesting"""
+    try:
+        validate_args(body[0].keys(), keys)
+    except MissingArguments:
+        return {'error': 'Please provide at least one argument (key).'}
+    except InvalidArgument:
+        return {'error': 'Invalid arguments. Arguments have to match JSON keys.'}
+    except TooManyArguments:
+        return {'error': 'More arguments than JSON keys provided.'}
+    except AlreadyPassedArgument:
+        return {'error': 'Duplicate arguments provided.'}
     return rebuild_json(body, keys)
